56cf5dce9e5f65afd1024b1a5b6a61e1a8b0d432
Martynas Zilinskas <hello@martynas.me>
2025-09-02 01:07:59 +0300

    Project init.

---
+++ src/app/layout.config.tsx
@@ -0,0 +1,32 @@
+import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import { SiFacebook } from "@icons-pack/react-simple-icons";
+
+export const baseOptions: BaseLayoutProps = {
+  githubUrl: "https://github.com/atvirastinklas/web",
+  nav: {
+    title: (
+      <>
+        <svg
+          width="24"
+          height="24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-label="Logo"
+        >
+          <title>Logo</title>
+          <circle cx={12} cy={12} r={12} fill="currentColor" />
+        </svg>
+        Atviras Kodas
+      </>
+    ),
+  },
+  // see https://fumadocs.dev/docs/ui/navigation/links
+  links: [
+    {
+      type: "icon",
+      icon: <SiFacebook />,
+      label: "Facebook Grupė",
+      text: "Facebook Grupė",
+      url: "https://www.facebook.com/groups/1122509422249414",
+    },
+  ],
+};
---
+++ src/app/layout.tsx
@@ -0,0 +1,35 @@
+import "@/app/global.css";
+import type { Translations } from "fumadocs-ui/i18n";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+
+const defaultTranslations: Translations = {
+  search: "Paieška",
+  searchNoResult: "Rezultatų nerasta",
+  toc: "Turinys",
+  tocNoHeadings: "Nėra antraščių",
+  lastUpdate: "Paskutinį kartą atnaujinta",
+  chooseLanguage: "Pasirinkite kalbą",
+  nextPage: "Kitas puslapis",
+  previousPage: "Ankstesnis puslapis",
+  chooseTheme: "Tema",
+  editOnGithub: "Redaguoti GitHub",
+};
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <body className="flex flex-col min-h-screen">
+        <RootProvider i18n={{
+          locale: "lt",
+          translations: defaultTranslations,
+        }}>{children}</RootProvider>
+      </body>
+    </html>
+  );
+}
