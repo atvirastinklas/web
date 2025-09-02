3f0c023cf280664663f4b514233eae043f2642df
Martynas Zilinskas <hello@martynas.me>
2025-09-03 01:11:37 +0300

    Added metadata for main page.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,11 @@
 import { Hero } from "@/components/landing/hero";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Atviras Tinklas",
+  description:
+    "Mes siekiame sukurti atvirą ryšio tinklą, kuriuo galėtų naudotis visi - tiek nelaimės atveju, tiek paprastam bendravimui. Nepriklausoma sistema, veikianti be interneto ir mobiliojo ryšio.",
+};
 
 export default function HomePage() {
   return (
