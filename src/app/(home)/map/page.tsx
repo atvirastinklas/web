4fab1a8067e7940f1dd246dc89ecb1b0f11087b9
Martynas Zilinskas <hello@martynas.me>
2025-09-04 23:48:14 +0300

    Quick fix.

--- src/app/(home)/map/page.tsx
+++ src/app/(home)/map/page.tsx
@@ -3,13 +3,9 @@ import { WorldMap } from "./world-map";
 export const dynamic = "force-dynamic";
 
 export default async function Page() {
-    // TODO: Refactor to use API instead.
-    const response = await fetch(`https://${process.env.NEXT_PUBLIC_DOMAIN}/nodes.json`);
-    const nodes = await response.json();
-
     return (
         <main className="h-full w-full">
-            <WorldMap nodes={nodes} />
+            <WorldMap />
         </main>
     );
 }
--- src/app/(home)/map/world-map.tsx
+++ src/app/(home)/map/world-map.tsx
@@ -1,7 +1,6 @@
 "use client";
-import MapComp, { FullscreenControl, GeolocateControl, Marker, NavigationControl, ScaleControl } from 'react-map-gl/maplibre';
+import MapComp, { FullscreenControl, GeolocateControl, Layer, type LayerProps, NavigationControl, ScaleControl, Source } from 'react-map-gl/maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css';
-import { Pin } from './pin';
 
 export interface MeshNode {
     nodeNum: number;
@@ -14,11 +13,7 @@ export interface MeshNode {
     lastUpdate: string;
 }
 
-interface Props {
-    nodes: MeshNode[];
-}
-
-export const WorldMap = (props: Props) => {
+export const WorldMap = () => {
     return (
         <MapComp
             initialViewState={{
@@ -33,11 +28,26 @@ export const WorldMap = (props: Props) => {
             <NavigationControl position="top-left" />
             <ScaleControl />
 
-            {props.nodes.map((node) => (
-                <Marker key={node.nodeNum} longitude={node.longitude} latitude={node.latitude}>
-                    <Pin name={node.longName} />
-                </Marker>
-            ))}
+            <Source id="nodes" type="geojson" data="https://atvirastinklas.lt/nodes.json">
+                <Layer id="points" type="circle" paint={{
+                    'circle-radius': 6,
+                    'circle-color': '#73bf69',
+                    'circle-stroke-color': '#fff',
+                    'circle-stroke-width': 2,
+                }} />
+                <Layer id="labels" type="symbol" layout={{
+                    'text-field': ['get', 'longName'],
+                    'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
+                    'text-size': 12,
+                    'text-offset': [0, 1.2],
+                    'text-anchor': 'top'
+                }} paint={{
+                    'text-color': '#73bf69',
+                    'text-halo-color': '#000',
+                    'text-halo-width': 1,
+                    'text-halo-blur': 1
+                }} />
+            </Source>
         </MapComp>
     );
 }
