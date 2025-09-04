75f6b1d34ca87cf9c24b16f8e3bd4689e0d5b0b9
Martynas Zilinskas <hello@martynas.me>
2025-09-04 03:51:15 +0300

    Fixed data loading.

--- src/app/(home)/map/page.tsx
+++ src/app/(home)/map/page.tsx
@@ -4,7 +4,7 @@ export const dynamic = "force-dynamic";
 
 export default async function Page() {
     // TODO: Refactor to use API instead.
-    const response = await fetch("/nodes.json");
+    const response = await fetch(`https://${process.env.NEXT_PUBLIC_DOMAIN}/nodes.json`);
     const nodes = await response.json();
 
     return (
