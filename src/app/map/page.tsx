d2a53a595a178120d9b63061586f2193f4568e1b
Martynas Zilinskas <hello@martynas.me>
2025-09-07 00:26:00 +0300

    Added missing metadata for map page.

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,4 +1,11 @@
+import type { Metadata } from "next";
 import { MeshMap } from "./mesh-map";
+
+export const metadata: Metadata = {
+  title: "Žemėlapis | Atviras Tinklas",
+  description:
+    "Mes siekiame sukurti atvirą ryšio tinklą, kuriuo galėtų naudotis visi - tiek nelaimės atveju, tiek paprastam bendravimui. Nepriklausoma sistema, veikianti be interneto ir mobiliojo ryšio.",
+};
 
 interface Props {
   searchParams: Promise<{ node?: string }>;
